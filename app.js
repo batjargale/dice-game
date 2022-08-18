e0f49d7deaa5dc15d29aeff6fa0ac684de243661
batjargal <batjargale@gmail.com>
2022-08-18 17:50:47 +0800

    hold tovch iig ajillulah

--- app.js
+++ app.js
@@ -17,9 +17,7 @@ var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 // addEventListener нь тухай event-ийн нэрийн дагуу үйлдэл хийдэг
 //   function() нь anonymous function yum
-document.querySelector(".btn-hold").addEventListener("click", function () {
-  //  activePlayer===0?scores = [diceNumber, ]:scores = [, diceNumber];
-});
+
 document.querySelector(".btn-roll").addEventListener("click", function () {
   //Шооны аль талаараа буусныг хадгалах хувьсагч. 1-6 утгыг хувьсагчид санамсаргүйгээр хадгална.
   var diceNumber = Math.floor(Math.random() * 6) + 1;
@@ -33,17 +31,49 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
     roundScore = roundScore + diceNumber;
     document.getElementById("current-" + activePlayer).textContent = roundScore;
   } else {
-    // 1 буулгахад дараагийн тоглогч руу шилжих үед шоог харагдуулахгүй болгох
-    diceDom.style.display = "none";
-    // Буусан тооны утгыг харуулах
-    // Энэ тоглогчийн оноог 0 болгож, дараагийн тоглогчид шилжүүлэх
-    roundScore = 0;
-    document.getElementById("current-" + activePlayer).textContent = 0;
+    // Тоглогчийн ээлжийг солино
+    toglogchinEelj();
+  }
+});
 
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+// Hold товчны эвзнт
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  // Цуглуулсан оноог үндсэн оноо дээр нэмж өгнө.
+  scores[activePlayer] = scores[activePlayer] + roundScore;
 
-    // Улаан цэгийг шилжүүлэх
+  // Уг тоглогч хожсон эсэхийг шалгах буюу 100 оноог хүрсэн эсэх
+  if (scores[activePlayer] >= 10) {
+    document.getElementById("name-" + activePlayer).textContent = "WINNER !!!";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
     document.querySelector(".player-0-panel").classList.toggle("active");
     document.querySelector(".player-1-panel").classList.toggle("active");
   }
+
+  // HTML кодоос score- гэсэн утгыг хайна
+  document.getElementById("score-" + activePlayer).textContent =
+    scores[activePlayer];
+
+  // Тоглогчийн ээлжийг солино
+  toglogchinEelj();
 });
+
+// Шинээр тоглоом эхлүүлэх
+document.querySelector(".btn-new").addEventListener("click", function () {
+  alert("Hi");
+});
+
+// Тоглогчийн ээлжийг солих ** Дуудалтад хэрэглэх фц -- DRY - dont repeat yourself
+function toglogchinEelj() {
+  // 1 буулгахад дараагийн тоглогч руу шилжих үед шоог харагдуулахгүй болгох
+  diceDom.style.display = "none";
+  // Дараагийн тоглогчид шилжүүлж, оноог тэглэх
+  roundScore = 0;
+  // Буусан тооны утгыг харуулах
+  document.getElementById("current-" + activePlayer).textContent = 0;
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  // Улаан цэгийг шилжүүлэх
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+}
