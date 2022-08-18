7fe0ea7dbd6e3450f73c224dc4b34bf5263bc030
batjargal <batjargale@gmail.com>
2022-08-18 15:21:20 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -5,23 +5,22 @@ var activePlayer = 1; //2-р тоглогч
 var scores = [0, 0];
 //Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
-
-//Шооны аль талаараа буусныг хадгалах хувьсагч. 1-6 утгыг хувьсагчид санамсаргүйгээр хадгална.
-var dice = Math.floor(Math.random() * 6) + 1;
-
-/* <div class="player-score" id="score-0">43</div> 
-1. id -ийн утгыг олохдоо Javascript дээр window.document.querySelector("#.... ") бичнэ.
-2. Түүний тестийн утга буюу 43-ийг өөрчлөхдөө   .textContent=   гэж бичнэ.
-3. Хэрэв дотор нь HTML бичихийг хүсвэл .innerHTML
-*/
-// window.document.querySelector("#score-0").textContent = dice;
-// document.querySelector("#score-1").textContent = dice;
-
 // Програм эхлэхэд бэлтгэе
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
-
-console.log("Шоо: " + dice);
+// id -р элементийг хайж олох  -- Илүү хурдан ажилладаг.
+// document.getElementById("score-0").textContent=0;
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+// addEventListener нь тухай event-ийн нэрийн дагуу үйлдэл хийдэг
+//   function() нь anonymous function yum
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  //Шооны аль талаараа буусныг хадгалах хувьсагч. 1-6 утгыг хувьсагчид санамсаргүйгээр хадгална.
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.src = "dice-" + diceNumber + ".png";
+  diceDom.style.display = "block";
+  //   alert("Шоо: " + diceNumber);
+});
